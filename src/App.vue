d385d4603437be96942a1298ae6b64272078f538
Kartar Singh <k4kartar4520@gmail.com>
2023-10-03 16:25:44 +0530

    dynamic components usage

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
-<HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <!-- <HomeView homeMsg="This is Home Component" /> -->
   <!-- <LoginView/> -->
+
+  <button @click="tab='LoginView'">LoginView</button>
+  <button @click="tab='HelloWorld'">HelloWorld</button>
+  <button @click="tab='HomeView'">HomeView</button>
+
+  <component :is="tab"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-// import HomeView from './components/HomeView.vue'
-// import LoginView from './components/LoginView.vue';
+import HomeView from './components/HomeView.vue'
+import LoginView from './components/LoginView.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    // HomeView,
-    // LoginView,
+    HomeView,
+    LoginView,
+  },
+  data(){
+    return{
+      tab:"LoginView"
+    }
   }
 }
 </script>
